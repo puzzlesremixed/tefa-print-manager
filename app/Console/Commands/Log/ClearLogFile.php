7c1e579591ec38cd833bc371b76fab3e34a6befa
kat <143475611+puzzlesremixed@users.noreply.github.com>
2026-02-12 11:44:39 +0700

    add clear log command

---
+++ app/Console/Commands/Log/ClearLogFile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Console\Commands\Log;
+
+use Illuminate\Console\Command;
+
+class ClearLogFile extends Command
+{
+
+  /**
+   * Execute the console command.
+   */
+
+
+  // Define command name
+  protected $signature = 'log:clear';
+
+  // Add description to your command
+  protected $description = 'Clear Laravel log';
+
+  // Create your own custom command
+  public function handle()
+  {
+    exec('echo "" > ' . storage_path('logs/laravel.log'));
+    $this->info('Logs have been cleared');
+  }
+
+}
