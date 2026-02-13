2dd4ce65fce0329a46cee970a4f389ae77f7f9ed
kat <143475611+puzzlesremixed@users.noreply.github.com>
2026-02-14 03:02:23 +0700

    fix: icon to use jsx syntax

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -10,10 +10,10 @@ export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
+          strokeWidth="2"
            
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
